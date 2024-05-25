a4372439d616541009868891375407f335057ddc
Kuang Yu Heng <yuheng3107@gmail.com>
2024-05-25 18:40:25 +0800

    Made the chart successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 function App() {
-  return (
-    <div className="text-xl text-red-500">Hello</div>
-  )
+  return <div className="text-xl text-red-500">Hello</div>;
 }
 
-export default App
+export default App;
