9fce15ca5ede60445a3d9a8b44f4a16b9b6413c8
Ming Jun <evan.zhangmingjun@gmail.com>
2024-05-25 20:06:07 +0800

    feat: add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import NavbarAndSidebar from "@/components/layouts/NavbarAndSidebar";
+import { Route, Routes } from "react-router-dom";
+import Home from "@/components/pages/Home";
+import Profile from "@/components/pages/Profile";
+
 function App() {
-  return <div className="text-xl text-red-500">Hello</div>;
+  return (
+    <NavbarAndSidebar>
+      <Routes>
+        {/* Single catch-all route because we only have one page now */}
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </NavbarAndSidebar>
+  );
 }
 
 export default App;
---
+++ src/components/pages/Profile.tsx
@@ -0,0 +1,9 @@
+function Profile() {
+  return (
+    <div className="w-full bg-neutral-100 p-8 flex flex-col">
+      <h1 className="text-3xl font-semibold text-neutral-900">Profile</h1>
+    </div>
+  );
+}
+
+export default Profile;
